7adce3f8dbc326aecdc557855d9426ab6c8e401b
Nigel Mulholland <nigeldave19@gmail.com>
2022-07-05 15:17:51 +0530

    updates

--- cypress/integration/Contract_Test/Contract/ContractSteps.js
+++ cypress/integration/Contract_Test/Contract/ContractSteps.js
@@ -14,7 +14,19 @@ And('User click on the {string} link',(text)=>{
 });
 
 And('User enter details as below:',(dataTable)=>{
-    dataTable.hashes().forEach(element => {
-        
-    });
+
+    const dataArray = dataTable.raw()
+    const transposedDataArray = dataArray[0].map((_, colIndex) => dataArray.map(row => row[colIndex]));
+
+    for(let i = 0; i < transposedDataArray.length; i++) {
+
+        let key   = transposedDataArray[i][0]
+        let value = transposedDataArray[i][1]
+
+        console.log("Key   ==> " + key)
+        console.log("Value ==> " + value) 
+
+        basePage.enter_value(key,value)
+    }
+
 });
--- cypress/support/page-objects/Base_Page.js
+++ cypress/support/page-objects/Base_Page.js
@@ -1,11 +1,110 @@
-export default class BasePage{
-    
+export default class BasePage {
+
     elements = {
-        a_normalize_link : (text) => cy.xpath('//a[normalize-space()=\'' + text + '\']'),
+        a_normalize_link: (text) => cy.xpath('//a[normalize-space()=\'' + text + '\']'),
+        input_field: (text, index) => cy.xpath('(//*[contains(text(),\'' + text + '\')])[' + index + ']/following-sibling::input[1]'),
+        textarea_field: (text, index) => cy.xpath('(//*[contains(text(),\'' + text + '\')])[' + index + ']/following-sibling::textarea[1]')
+
     }
 
-    clickOnNormalizeLink(text){
+    clickOnNormalizeLink(text) {
         this.elements.a_normalize_link(text).click();
     }
-    
+
+    enterValueInput(question, answer) {
+        if (question.match('.*\\[[\\d.]]')) {
+            let valueAndIndex = getValueAndIndex(question)
+            console.log("Field_name : " + valueAndIndex[0])
+            console.log("Index : " + valueAndIndex[1])
+            this.elements.input_field(valueAndIndex[0], valueAndIndex[1]).type(answer);
+        } else {
+            console.log("Field_name : " + question)
+            console.log("Index : 1")
+            this.elements.input_field(question, 1).type(answer);
+        }
+    }
+
+    enterValueTextArea(question, answer) {
+        if (question.match('.*\\[[\\d.]]')) {
+            let valueAndIndex = getValueAndIndex(question)
+            console.log("Field_name : " + valueAndIndex[0])
+            console.log("Index : " + valueAndIndex[1])
+            this.elements.textarea_field(valueAndIndex[0], valueAndIndex[1]).type(answer);
+        } else {
+            let valueAndIndex = getValueAndIndex(question)
+            console.log("Field_name : " + valueAndIndex[0])
+            console.log("Index : 1")
+            this.elements.textarea_field(valueAndIndex[0], 1).type(answer);
+        }
+    }
+
+    enter_value(question, answer) {
+
+        console.log("Input ::: " + question)
+
+        switch (switch_option(question)) {
+
+            case 'suggest':
+                console.log('This is for Suggest tag')
+                if (question.match('.*\\[[\\d.]]')) {
+                    let valueAndIndex = getValueAndIndex(question)
+                    console.log("Field_name : " + valueAndIndex[0])
+                    console.log("Index : " + valueAndIndex[2])
+                } else {
+                    let valueAndIndex = getValueAndIndex(question)
+                    console.log("Field_name : " + valueAndIndex[0])
+                    console.log("Index : 1")
+                }
+                break;
+
+            case 'textarea':
+                console.log('This is for Text Area tag')
+                this.enterValueTextArea(question, answer)
+                break;
+
+            case 'date':
+                console.log('This is for Date tag')
+                if (question.match('.*\\[[\\d.]]')) {
+                    let valueAndIndex = getValueAndIndex(question)
+                    console.log("Field_name : " + valueAndIndex[0])
+                    console.log("Index : " + valueAndIndex[2])
+                } else {
+                    let valueAndIndex = getValueAndIndex(question)
+                    console.log("Field_name : " + valueAndIndex[0])
+                    console.log("Index : 1")
+                }
+                break;
+
+            default:
+                console.log('This is for Input tag')
+                this.enterValueInput(question, answer)
+                break;
+        }
+    }
+
+
+}
+
+function getValueAndIndex(value) {
+
+    if (value.includes('[')) {
+        let values = value.split('[')
+        let result = values.map(x => x.replace(']', ''));
+        return result
+    } else {
+        return value
+    }
+
+}
+
+function switch_option(text) {
+
+    let valueAndIndex = getValueAndIndex(text)
+
+    if (valueAndIndex.length > 1) {
+        return getValueAndIndex(text)[1]
+    } else {
+        return getValueAndIndex(text)[0]
+    }
+
 }
